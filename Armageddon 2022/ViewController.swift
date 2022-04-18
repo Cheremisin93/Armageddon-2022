072da27236ead27be339aa5c7f549a7c3b5c012e
Cheremisin93 <cheremisin9393@mail.ru>
2022-04-18 22:09:21 +0300

    Initial Commit

---
+++ Armageddon 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Armageddon 2022
+//
+//  Created by Cheremisin Andrey on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
